cb94a45d0475bd7c5719f7cc029c23b540fe8146
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 18:25:41 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -11,7 +11,6 @@ languages.each do |function, lang_type|
   lang_type.each do |lang, type|
     curr_function << [{function => lang}]
     puts curr_function
-    new_new_hash = new_hash
   end
 end
 puts new_hash
