11027ad7f8d20ccff06f5431388488451c3bf75f
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 18:58:33 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,9 @@ def reformat_languages(languages)
   grouped.each do |grouped_k, grouped_v|
     grouped_k.each do |grouped_first, grouped_second|
       grouped_second.each do |second, rest|
+        puts second
     end
+  end
   end
 end
 
