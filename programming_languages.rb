725be5498fe1526b20169dfdf6f29bcead1bfcf7
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 19:03:45 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
 new_hash_type = {}
 curr_function = []
 new_hash_style = {}
+new_hash = {}
 
   languages.each do |function, lang_type|
     lang_type.each do |lang, type|
