bc5b91fd60649d075de42fd802be7cf219b503e8
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 18:21:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,45 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    new_hash_type = {}
+    new_hash_style = {}
+    curr_function = []
+
+languages.each do |function, lang_type|
+  curr_function = []
+  lang_type.each do |lang, type|
+    new_hash = new_hash[function] => lang
+    new_new_hash = new_hash
+  end
 end
+puts new_hash
+end
+
+=begin
+  languages.each do |function, lang_type|
+    lang_type.each do |lang, type|
+      type.each do |key, value|
+        new_hash_type[lang] = type
+            if curr_function.include?(function)
+              curr_function
+            else
+               curr_function << function
+             end
+      new_hash_style[lang] = {:style =>curr_function}
+      puts new_hash_style
+    end
+    end
+  end
+  new_hash_style.each do |style_lang, style_style|
+    style_style.each do |style_key, style_value|
+    new_hash_type.each do |type_lang, type_type|
+      type_type.each do |type_key, type_value|
+      if style_lang == type_lang
+        new_hash[style_lang] = {:type => type_value, :style => style_value}
+      end
+    end
+end
+end
+end
+return new_hash
+end
+=end
