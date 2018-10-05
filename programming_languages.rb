ead9417e44647673f4c1734ea9cbe45783d89141
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 18:29:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ languages.each do |function, lang_type|
   lang_type.each do |lang, type|
     curr_function << [{function => lang}]
   end
-  puts curr_function.to_h
+  puts curr_function
 end
 
 end
