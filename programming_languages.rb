5426f3188fab3bc1536af0b05e592ce887237cf9
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 18:29:02 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,8 +9,7 @@ languages.each do |function, lang_type|
   lang_type.each do |lang, type|
     curr_function << [{function => lang}]
   end
-  grouped = curr_function.group_by {|el| el}
-  puts grouped
+  puts curr_function.to_h
 end
 
 end
