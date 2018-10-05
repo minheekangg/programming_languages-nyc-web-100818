7417a44573c7836e1af39d596e4e8a4f7e306ebe
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 19:05:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,11 +6,11 @@ new_hash_style = {}
 new_hash = {}
 
   languages.each do |function, lang_type|
-    puts function
     lang_type.each do |lang, type|
       type.each do |key, value|
         new_hash_type[lang] = type
           curr_function = []
+          puts function
             if curr_function.include?(function)
               curr_function
             else
